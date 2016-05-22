c4c6f0a926e26bc1aef9f6db17f5bcaa5cd2ce11
Ben Miller <ben.miller@cohortsolutions.com>
2016-05-22 17:00:22 +1000

    remove stray pry call

--- app/services/issue_service.rb
+++ app/services/issue_service.rb
@@ -1,6 +1,5 @@
 class IssueService
   def self.create_issue(controller, problem, user)
-    binding.pry
     issue = Issue.new(problem: problem, user: user)
     issue.body = controller.render_to_string(*issue.render_body_args)
     issue.save
